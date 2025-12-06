cc3a7130433daf77b2fe3f8427e4e9ea068baf0e
Ephy <macbookpro@Ephys-MacBookPro.local>
2025-12-06 12:16:18 +0300

    donewithfile

---
+++ us-time-frontend/src/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// Create root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
